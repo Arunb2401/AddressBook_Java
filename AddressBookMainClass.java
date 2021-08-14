d72890744d5e3edb036a8f088d1dbaf0d1491123
Arunb2401 <arunbiradar24@gmail.com>
2021-08-14 12:18:56 +0530

    Added: Welcome to Address Book Program.

---
+++ AddressBookMainClass.java
@@ -0,0 +1,10 @@
+package addressBook;
+
+import java.util.Scanner;
+
+public class AddressBookMainClass {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		
+	}
+}
